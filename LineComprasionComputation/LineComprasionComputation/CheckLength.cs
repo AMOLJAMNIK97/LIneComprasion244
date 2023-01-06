9cfde688a9db7641f180faf0b36a7dc4b0307161
Amoljamnik97 <ajamnik.mech@gmail.com>
2023-01-06 21:12:11 +0530

    [amol]- Check Two line Equal or Not

---
+++ LineComprasionComputation/LineComprasionComputation/CheckLength.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComprasionComputation
+{
+    public class CheckLength
+    {
+        public double LengthOfLine(int x1, int x2, int y1, int y2)
+        {
+            double lengthOfLine = Math.Sqrt((Math.Pow(x2 - x1, 2)) + Math.Pow(y2 - y1, 2));
+            return lengthOfLine;
+        }
+
+        public void checkEquality(double lengthofLine1, double lengthOfLine2)
+        {
+            if (lengthofLine1 == lengthOfLine2)
+            {
+                Console.WriteLine("Both lines are equal");
+            }
+            else
+            {
+                Console.WriteLine("Both lines are not equal");
+            }
+        }
+        public void LengthOfLine1()
+        {
+            int x1, x2, m1, m2, y1, y2, n1, n2;
+            Console.WriteLine("Enter the value for x1 and x2 co-ordinate for Line1");
+            x1 = Convert.ToInt32(Console.ReadLine());
+            x2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the value for y1 and y2 co-ordinate for Line1");
+            y1 = Convert.ToInt32(Console.ReadLine());
+            y2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the value for m1 and m2 co-ordinate for Line2");
+            m1 = Convert.ToInt32(Console.ReadLine());
+            m2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the value for n1 and n2 co-ordinate for line2");
+            n1 = Convert.ToInt32(Console.ReadLine());
+            n2 = Convert.ToInt32(Console.ReadLine());
+
+            CheckLength Cal = new CheckLength();
+            double lengthOfLine1 = Cal.LengthOfLine(x1, x2, y1, y2);
+            double lengthOfLine2 = Cal.LengthOfLine(m1, m2, n1, n2);
+            Console.WriteLine("Length of 1st line end points::{0}", lengthOfLine1);
+            Console.WriteLine("Length of 2nd line end points::{0}", lengthOfLine2);
+            Cal.checkEquality(lengthOfLine1, lengthOfLine2);
+            Console.ReadLine();
+        }
+    }
+}
