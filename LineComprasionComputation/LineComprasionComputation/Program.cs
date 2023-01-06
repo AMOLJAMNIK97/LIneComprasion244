4db6a2c90c5ca0fdddd5f1304a67dcd70df8eb4a
Amoljamnik97 <ajamnik.mech@gmail.com>
2023-01-06 21:18:37 +0530

    [amol]- Compare Two line

--- LineComprasionComputation/LineComprasionComputation/Program.cs
+++ LineComprasionComputation/LineComprasionComputation/Program.cs
@@ -12,14 +12,23 @@ namespace LineComprasionComputation
  
         static void Main(string[] args)
         {
-            CalCulateLength CalCulate = new CalCulateLength(); 
+             
             Console.WriteLine("Welcome to Line Comparison Computation");
-            Console.WriteLine("Choose option\n 1.Calculate Length");
+            Console.WriteLine("Choose option\n 1.Calculate Length\n2.Check Two Line Equal\n3.Compare Two Line");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
             {
                 case 1:
+                    CalCulateLength CalCulate = new CalCulateLength();
                     CalCulate.calcalulatelength();
+                    break;
+                case 2:
+                    CheckLength EqualTwoLine = new CheckLength();
+                    EqualTwoLine.LengthOfLine1();
+                    break;
+                case 3:
+                    CompareTwoLine compare = new CompareTwoLine();
+                    compare.CheckBothLine();
                     break;
                 default:
                     Console.WriteLine("Wrong choice");
