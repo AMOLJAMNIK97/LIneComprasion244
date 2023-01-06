b35b1e0175628be71f49f8a0c3f0a8389936015f
Amoljamnik97 <ajamnik.mech@gmail.com>
2023-01-06 20:47:38 +0530

    [amol]- Calculate Length

--- LineComprasionComputation/LineComprasionComputation/Program.cs
+++ LineComprasionComputation/LineComprasionComputation/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComprasionComputation
+{
+
+    class Program
+    {
+ 
+        static void Main(string[] args)
+        {
+            CalCulateLength CalCulate = new CalCulateLength(); 
+            Console.WriteLine("Welcome to Line Comparison Computation");
+            Console.WriteLine("Choose option\n 1.Calculate Length");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    CalCulate.calcalulatelength();
+                    break;
+                default:
+                    Console.WriteLine("Wrong choice");
+                    break;
+            }
+        }
+    }
+}
